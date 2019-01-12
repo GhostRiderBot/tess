1ab3f640f08c13e47e02bfa43140fdd979aaa995
GhostRiderBot <37671119+GhostRiderBot@users.noreply.github.com>
2019-01-12 08:20:54 +0700

    Add files via upload

---
+++ Pro/ baris/Api/__init__.py
@@ -0,0 +1,5 @@
+from .Talk import Talk
+from .Poll import Poll
+from .channel import Channel
+from .server import Server
+from .call import Call
---
+++ Pro/ baris/Api/call.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+from thrift.transport import THttpClient
+from thrift.protocol import TCompactProtocol
+from .config import Config
+from akad import CallService
+#from akad.ttypes import GroupCallMediaType
+from akad.ttypes import *
+
+class Call(Config):
+    client = None
+
+    def __init__(self, authToken):
+       Config.__init__(self)
+       self.transport = THttpClient.THttpClient(self.LINE_HOST_DOMAIN, None, self.LINE_API_QUERY_PATH_FIR)
+       self.transport.path = self.LINE_AUTH_QUERY_PATH
+       self.transport.setCustomHeaders({"X-Line-Application" : self.APP_NAME,"User-Agent" : self.USER_AGENT,"X-Line-Access": authToken})
+       self.protocol = TCompactProtocol.TCompactProtocol(self.transport);
+       self.client = CallService.Client(self.protocol)
+       self.transport.path = self.LINE_CALL_QUERY_PATH
+       self.transport.open()
+
+
+    def acquireCallRoute(self,to):
+        return self.client.acquireCallRoute(to)
+
+    def acquireGroupCallRoute(self, groupId, mediaType=GroupCallMediaType.AUDIO):
+        return self.client.acquireGroupCallRoute(groupId, GroupCallMediaType)
+
+    def getGroupCall(self, ChatMid):
+        return self.client.getGroupCall(ChatMid)
+
+    def inviteIntoGroupCall(self, chatId, contactIds=[], mediaType=GroupCallMediaType.AUDIO):
+        return self.client.inviteIntoGroupCall(chatId, contactIds, GroupCallMediaType)
